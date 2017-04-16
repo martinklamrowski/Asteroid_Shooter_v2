7083cf91d4516d85569319862886b95c56ee5750
fifiman <bauk.nenad@gmail.com>
2017-04-16 23:36:56 +0200

    Clean up game config.

--- game/config.py
+++ game/config.py
@@ -3,8 +3,7 @@ Config file that holds constants used throught the program.
 Can probably be done much better than this.
 """
 
-
+# Game config
 screenWidth = 1000
 screenHeight = 700
-
 framerate = 60
