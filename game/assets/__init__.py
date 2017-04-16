d13bf57d0cca2b388a6c791a673b107dffcbc17b
fifiman <bauk.nenad@gmail.com>
2017-04-16 21:35:19 +0200

    Add ship assets

---
+++ game/assets/__init__.py
@@ -0,0 +1,9 @@
+"""
+Contains all image objects for the game.
+"""
+
+import pygame
+
+# Ship assets
+shipImage = pygame.image.load('ship/ship.png')
+transparentShipImage = pygame.image.load('ship/transparentShip.png')
