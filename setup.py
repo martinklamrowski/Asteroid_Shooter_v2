11b07b9326044ae4274021b773b9c45acdf152da
fifiman <bauk.nenad@gmail.com>
2017-06-02 16:36:53 +0200

    Name change because of pypi.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name = 'Asteroid-Shooter-v2.0',
-  packages = ['Asteroid-Shooter-v2.0'],
+  name = 'Asteroid-Shooter-v2',
+  packages = ['Asteroid-Shooter-v2'],
   version = '1.0',
   description = 'The new and improved version to my first video game: an asteroid shooter.',
   author = 'Nenad Bauk',
   author_email = 'bauk.nenad@gmail.com',
-  url = 'https://github.com/fifiman/Asteroid-Shooter-v2.0',
-  download_url = 'https://github.com/fifiman/Asteroid-Shooter-v2.0/archive/1.0.tar.gz',
+  url = 'https://github.com/fifiman/Asteroid-Shooter-v2',
+  download_url = 'https://github.com/fifiman/Asteroid-Shooter-v2/archive/1.0.tar.gz',
   keywords = ['game', 'python', 'pygame'],
   classifiers = [],
 )
