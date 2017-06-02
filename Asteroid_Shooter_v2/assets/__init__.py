3eae33119b272adf03e73c6e1de48ea4a1d45c4a
fifiman <bauk.nenad@gmail.com>
2017-06-02 16:52:26 +0200

    Fix file names.

--- Asteroid_Shooter_v2/assets/__init__.py
+++ Asteroid_Shooter_v2/assets/__init__.py
@@ -5,38 +5,38 @@ Contains all image objects for the game.
 import pygame
 
 # Ship assets
-shipImageDir = 'game/assets/ship/ship.png'
-transparentShipImageDir = 'game/assets/ship/transparentShip.png'
+shipImageDir = 'Asteroid_Shooter_v2/assets/ship/ship.png'
+transparentShipImageDir = 'Asteroid_Shooter_v2/assets/ship/transparentShip.png'
 
 shipImage = pygame.image.load(shipImageDir)
 transparentShipImage = pygame.image.load(transparentShipImageDir)
 
-bulletImageDir = 'game/assets/ship/bullet.png'
+bulletImageDir = 'Asteroid_Shooter_v2/assets/ship/bullet.png'
 bulletImage = pygame.image.load(bulletImageDir)
 
-heartImageDir = 'game/assets/ship/heart16.png'
+heartImageDir = 'Asteroid_Shooter_v2/assets/ship/heart16.png'
 heartImage = pygame.image.load(heartImageDir)
 
 # Asteroid assets
-asteroidImageDir = 'game/assets/asteroid/asteroid.png'
+asteroidImageDir = 'Asteroid_Shooter_v2/assets/asteroid/asteroid.png'
 asteroidImage = pygame.image.load(asteroidImageDir)
 
-homingAsteroidImageDir = 'game/assets/asteroid/homing_asteroid.png'
+homingAsteroidImageDir = 'Asteroid_Shooter_v2/assets/asteroid/homing_asteroid.png'
 homingAsteroidImage = pygame.image.load(homingAsteroidImageDir)
 
 # Powerup assets
-bulletSpamImageDir = 'game/assets/powerups/bolt16.png'
+bulletSpamImageDir = 'Asteroid_Shooter_v2/assets/powerups/bolt16.png'
 bulletSpamImage = pygame.image.load(bulletSpamImageDir)
 
-wideBulletImageDir = 'game/assets/powerups/wide20.png'
+wideBulletImageDir = 'Asteroid_Shooter_v2/assets/powerups/wide20.png'
 wideBulletImage = pygame.image.load(wideBulletImageDir)
 
 # Music
-backgroundSoundDir = 'game/assets/music/background.ogg'
-bulletSoundDir = 'game/assets/music/bullet.ogg'
-shipHitSoundDir = 'game/assets/music/hit.ogg'
+backgroundSoundDir = 'Asteroid_Shooter_v2/assets/music/background.ogg'
+bulletSoundDir = 'Asteroid_Shooter_v2/assets/music/bullet.ogg'
+shipHitSoundDir = 'Asteroid_Shooter_v2/assets/music/hit.ogg'
 
 # Ship explosion images
-shipExplosionDirPref = 'game/assets/gameover/explosion'
+shipExplosionDirPref = 'Asteroid_Shooter_v2/assets/gameover/explosion'
 shipExplosionDirs = [shipExplosionDirPref + '%i.png' % i for i in range(1, 11)]
 shipExplosionImages = [pygame.image.load(path) for path in shipExplosionDirs]
